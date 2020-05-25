02148dea941c931a056c5d5283e4d91e834f703d
viviantom0506 <icemarina123@gmail.com>
2020-05-24 20:36:18 -0400

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -4,10 +4,10 @@ function fizzBuzz(start, end) {
     // Insert code here;
     var array = [];
     for(var i = start; i <= end; i++) {
-      if(i % 3 === 0) {
+      if(i % 3 === 0 && i % 5 != 0) {
         array.push("Fizz");
       }
-       if(i % 5 === 0) {
+       if(i % 5 === 0 && i % 3 != 0) {
         array.push("Buzz");
       }
       if(i % 3 === 0 && i % 5 === 0) {
