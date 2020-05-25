d838a1eb8823f7ca60ea7ea0261ccd08da78801a
viviantom0506 <icemarina123@gmail.com>
2020-05-24 20:20:00 -0400

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -2,10 +2,11 @@ function sumOfAllEvenNumbers(nums) {
   // Insert code here;
   let num = [];
   var count = 0;
+  
   var i;
   for(i = 0; i < nums.length; i++) {
     if(nums[i] %2 === 0) {
-      sum += i;
+  
       count++;
     }
   }
