00cf2a54d133c9e45a0684eadc51af75e2e6d48a
viviantom0506 <icemarina123@gmail.com>
2020-05-25 12:33:40 -0400

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,16 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  // Split the string into an array containing each word seperated by spaces
+  let titleCase = title.split(" ");
+
+  // Capitalize the first character of the string in each index
+  for (let i=0; i<titleCase.length; i++) {
+    titleCase[i] = titleCase[i].charAt(0).toUpperCase() + titleCase[i].substr(1);
+  }
+
+  // Return a string of the array by using .join
+  return titleCase.join(' ');
 }
+
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
