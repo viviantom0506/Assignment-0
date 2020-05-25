96d17b3f6e11f9714c4562a88ae7e86a373689a6
viviantom0506 <icemarina123@gmail.com>
2020-05-25 12:31:34 -0400

    Update countOfAllIndexMatchingNumbers.js

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,14 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  let num = [];
+  var count = 0;
+  for(var i = 0; i < nums.length; i++) {
+    if(i === nums[i]) {
+      count++;
+    }
+    
+  }
+  return count;
 }
 
 // Do not edit this line;
