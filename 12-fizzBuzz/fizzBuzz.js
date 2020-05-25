7a68412190aa26f69010ef3b225cb0f4062864db
viviantom0506 <icemarina123@gmail.com>
2020-05-24 20:32:53 -0400

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -7,19 +7,18 @@ function fizzBuzz(start, end) {
       if(i % 3 === 0) {
         array.push("Fizz");
       }
-      else if(i % 5 === 0) {
+       if(i % 5 === 0) {
         array.push("Buzz");
       }
-      else if(i % 3 === 0 && i % 5 === 0) {
+      if(i % 3 === 0 && i % 5 === 0) {
         array.push("FizzBuzz");
       }
-      else if(i % 3 != 0 && i % 5 != 0) {
+      if(i % 3 != 0 && i % 5 != 0) {
         array.push(i);
       }
     }
     return array;
   }
-
 
 // Do not edit this line;
 module.exports = fizzBuzz;
