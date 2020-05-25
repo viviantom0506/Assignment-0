df91279505b9dccf1035dc456cf6411919d16c9e
viviantom0506 <icemarina123@gmail.com>
2020-05-25 18:39:02 -0400

    Update countDownSum.js

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,8 +1,13 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    if(num === 1) {
+      return 1;
+    }
+    return num + this.countDownSum(num-1);
   }
 }
+
 
 // Do not edit this line;
 let StudentSolution = MySolution;
