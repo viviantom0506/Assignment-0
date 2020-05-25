ceff078a902e7e77b7b3542d6cb734e4e4b0b14f
viviantom0506 <icemarina123@gmail.com>
2020-05-25 12:04:37 -0400

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var count = 0;
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] === true || arr[i] === false || typeof arr[i] === 'string') {
+      count++;
+    }
+  }
+  return count;
 }
+
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
