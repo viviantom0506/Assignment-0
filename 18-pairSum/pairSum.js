e88c71df795ea570a105567b98f743b748025750
viviantom0506 <icemarina123@gmail.com>
2020-05-25 21:01:29 -0400

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,28 @@
 function pairSum(nums, target) {
   // Insert code here;
+   let num = [];
+   var sum = 0;
+   if(nums.length <=1) {
+    throw "Array should be more than 1";
+  }
+   for(var i = 0; i < nums.length; i++) {
+     for(var j = i+1; j <nums.length; j++) {
+     
+   
+    if(target === nums[i]) {
+      return true;
+    }
+    else if((nums[i] + nums[j]) === target && i!=j) {
+      return true;
+    }
+   
+   
+   
+   
+  }
 }
+return false;
 
+}
 // Do not edit this line;
 module.exports = pairSum;
