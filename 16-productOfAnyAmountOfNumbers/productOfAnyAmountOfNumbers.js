7cb3f8e58142239ccb128584b11733dfe9bdda5c
viviantom0506 <icemarina123@gmail.com>
2020-05-25 20:27:16 -0400

    Add more solutions

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,11 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  var number = [];
+  var product = 1;
+  for(var i = 0; i < args.length; i++) {
+    product *= args[i];
+  }
+  return product;
 }
 
 // Do not edit this line;
--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let finalSet = new Set();
+  for(var i = 0; i < args.length; i++) {
+    for(let numbers of args[i]) {
+      finalSet.add(numbers);
+    }
+    
+  }
+  return finalSet;
 }
 
 // Do not edit this line;
