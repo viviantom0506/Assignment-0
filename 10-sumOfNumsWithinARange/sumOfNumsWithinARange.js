97caca4a37bf77a862b1dffa5a1f93213f279397
viviantom0506 <icemarina123@gmail.com>
2020-05-25 21:02:39 -0400

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -2,7 +2,7 @@ function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
   // var num = [];
   var count = 0;
-  for(var i = start; i< end; i++) {
+  for(var i = 0; i< nums.length; i++) {
     if(nums[i] >= start && nums[i] <= end) {
       count++;
     }
