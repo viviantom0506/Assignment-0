64947e11f43d638a833da8c7529876b8bdd9d88a
viviantom0506 <icemarina123@gmail.com>
2020-05-26 10:38:03 -0400

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,9 +5,27 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
-  }
-}
+    let beginning = 0; 
+    let endIndex = nums.length - 1;  
+    let middleIndex;    
 
+     
+    while (beginning <= endIndex){ 
+      middleIndex = Math.floor((beginning + endIndex) / 2);  
+      if(nums[middleIndex] === target){  
+        return true;
+
+      } else if (nums[middleIndex] > target){ 
+        endIndex = middleIndex - 1;
+
+      } else {
+        beginning = middleIndex + 1; 
+      }
+    }
+    return false; 
+
+  }  
+}
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
